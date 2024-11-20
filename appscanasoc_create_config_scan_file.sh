cc1379998042e64008face36ccb51d8cda0bc527
jrocia <69405400+jrocia@users.noreply.github.com>
2024-11-20 10:56:58 -0300

    Update appscanasoc_create_config_scan_file.sh

--- appscanasoc_create_config_scan_file.sh
+++ appscanasoc_create_config_scan_file.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "<?xml version="1.0" encoding="UTF-8" standalone="no"?><Configuration enableSecrets="true" sourceCodeOnly="true" staticAnalysisOnly="true"><Targets><Target path="$PWD"></Target></Targets></Configuration>" > appscan-config.xml
 diffFilesList=$(git diff --name-only HEAD HEAD~1)
-echo "antes do array $diffFilesList"
 readarray -t diffFiles <<< "$diffFilesList"
-echo "File1: ${diffFiles[0]}"
-echo "File2: ${diffFiles[1]}"
 for i in ${diffFiles[@]}; do sed -i "s|\(</Target>\)|<Include>$i</Include>\1|" appscan-config.xml; done
