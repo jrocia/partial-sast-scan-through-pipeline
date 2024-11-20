1101ca7ffcae248dccc256a8582193c37875bff7
jrocia <69405400+jrocia@users.noreply.github.com>
2024-11-20 09:53:28 -0300

    Update appscanasoc_create_config_scan_file.sh

--- appscanasoc_create_config_scan_file.sh
+++ appscanasoc_create_config_scan_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "<?xml version="1.0" encoding="UTF-8" standalone="no"?><Configuration enableSecrets="true" sourceCodeOnly="true" staticAnalysisOnly="true"><Targets><Target path="$PWD"></Target></Targets></Configuration>" > appscan-config.xml
 declare -a diffFiles
-diffFiles=(git diff --name-only HEAD HEAD~1)
+diffFiles=$(git diff --name-only HEAD HEAD~1)
 declare -p diffFiles
 echo "File1: $diffFiles[0]"
 echo "File2: $diffFiles[1]"
